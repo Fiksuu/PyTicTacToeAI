3abd86097b19af825197d0f78419283659cd10de
Fiksuu <piotrfiks560@gmail.com>
2024-06-01 19:19:43 +0200

    modified:   pttta.py

---
+++ pttta.py
@@ -0,0 +1,6 @@
+import sys #umożliwia zamknięcie aplikacji za pomocą klawisza
+import pygame #zewnętrzna biblioteka ułatwiajaca tworzenie gier
+
+#Kod Pygame
+pygame.init()
+screen = pygame.display.set_mode( (HEIGHT, WIDTH) )
