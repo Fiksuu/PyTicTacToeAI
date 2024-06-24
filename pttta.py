bef55e6e0eab37a29462387f06e86844a562b0a6
Fiksuu <piotrfiks560@gmail.com>
2024-06-25 01:17:29 +0200

    modified:   consts.py 	modified:   pttta.py

--- pttta.py
+++ pttta.py
@@ -2,13 +2,18 @@ import sys #umożliwia zamknięcie aplikacji za pomocą klawisza
 import pygame #zewnętrzna biblioteka ułatwiajaca tworzenie gier
 
 from consts import *
-#Ustawienie Pygame
+#Ustawienia Pygame
 pygame.init()
 screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption('Tic Tac Toe')
 screen.fill( BG_COLOUR )
 
 #Kod Pygame
+class Game: 
+    def __init__(self):
+        pass
+    def show
+
 def main():
     
     while True:
