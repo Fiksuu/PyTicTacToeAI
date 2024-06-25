5e09b9a96aecdab84fded3913a95fcc9b150f636
Fiksuu <piotrfiks560@gmail.com>
2024-06-26 01:54:37 +0200

    modified:   consts.py 	modified:   pttta.py

--- pttta.py
+++ pttta.py
@@ -15,30 +15,47 @@ class Board:
     def __init__(self):
         self.squares = np.zeros( (ROWS, COLS) )
 
-    def mark_sqrt(self, row, col, player):
-        self.squares[row] [col] = player
+    def mark_sqr(self, row, col, player):
+        self.squares[row][col] = player
+
+    def empty_sqr(self, row, col):
+        return self.squares[row][col] == 0
 
 class Game: 
     def __init__(self):
         self.board = Board()
-        self.player = 1
+        self.player = 1 #gracz 1 - krzyżyk || gracz 2 - kółko
         self.show_lines()
         
     def show_lines(self):
-        #pionowe
+        #pionowe linie
         pygame.draw.line(screen, LINE_COLOUR, (SQSIZE, 0), (SQSIZE, HEIGHT), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOUR, (WIDTH - SQSIZE, 0), (WIDTH - SQSIZE, HEIGHT), LINE_WIDTH)
 
-        #poziome
+        #poziome linie
         pygame.draw.line(screen, LINE_COLOUR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOUR, (0, HEIGHT - SQSIZE), (WIDTH, HEIGHT - SQSIZE), LINE_WIDTH)
+
+    def draw_fig(self, row, col):
+        if self.player == 1:
+            pass
+
+        elif self.player == 2:
+            pass
+        
+        #zmiana gracza
+    def next_player(self):
+        self.player = self.player % 2 + 1
 
 
 def main():
 
     #objekt
     game = Game()
-    
+    board = game.board
+
+
+    #główna pętla
     while True:
 
         for event in pygame.event.get():
@@ -46,8 +63,18 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            #wykrywanie kliknięć na planszy
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+                row = pos[1] // SQSIZE
+                col = pos[0] // SQSIZE
 
-            if event.type == pygame.MOUSEBUTTONDOWN
+                if board.empty_sqr(row, col):
+                    board.mark_sqr(row, col, game.player)
+                    game.draw_fig(row, col)
+                    game.next_player()
+                    
+                
 
         pygame.display.update()    
 
